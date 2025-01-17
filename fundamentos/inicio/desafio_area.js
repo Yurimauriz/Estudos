04e51f2187488dde457848e9b50bec465e49ad89
FRANCISCO YURI DA SILVA MAURIZ <yurimauriz.dev@gmail.com>
2025-01-17 17:40:41 -0300

    Desafios e conclusão do modulo fumdamentos

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,9 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592
+let raio = 10;
+let areaCircuferencia = PI * raio * raio;
+
+console.log("O valor da área da circunferência é: " + areaCircuferencia + "m2.");
+console.log(Math.PI * raio * raio);
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+// depois da troca... a = 94 e b = 7
+
+let A = a;
+
+a = b;
+b = A;
+console.log("a agora é " + a);
+console.log("b agora é " + b);
