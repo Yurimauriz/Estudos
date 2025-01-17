a6dd6d7b158cec753a10db3df1a3eb8725965e75
FRANCISCO YURI DA SILVA MAURIZ <yurimauriz.dev@gmail.com>
2025-01-17 16:36:05 -0300

    Aula Trabalhando com Dados#1

---
+++ fundamentos/inicio/dados_1.js
@@ -0,0 +1,15 @@
+// Variavel é lugar para amazenar uma informação, que é tbm um espaço de memória
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4
+var imposto = 1.5
+let precoFinal = preco + imposto; // let é uma forma mais moderna para criar uma variavel
+
+console.log(nome); // quando definido um valor direto é chamado de valor literal exemplo console.log(10)
+console.log(quantidade); 
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Ceneta BIC";
+console.log(nome); 
