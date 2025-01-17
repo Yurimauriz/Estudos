4fbd61a1cc2f8dbbef6db3d19e40cc5d26ce0048
FRANCISCO YURI DA SILVA MAURIZ <yurimauriz.dev@gmail.com>
2025-01-17 14:59:27 -0300

    aula sobre comentario

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,13 @@
+// console.log é usado para exibir mensagens no console do navegador.
+console.log('Passo #1');
+console.log('Passo #2'); // mais um exemplo de console.log
+// console.log('Passo #3');
+
+/*
+ *   Esse é um 
+ *   comentário
+ *   de múltiplas linhas
+ * 
+*/
+
+// Evitar comentario logico em projetos empresarial
