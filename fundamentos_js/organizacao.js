ed8344ec5ec0e5d9d257a6b51397509adc975bdb
FRANCISCO YURI DA SILVA MAURIZ <yurimauriz.dev@gmail.com>
2025-01-23 07:09:34 -0300

    teste

---
+++ fundamentos_js/organizacao.js
@@ -0,0 +1,11 @@
+let numero1 = 10
+let numero2 = 95
+
+soma = numero1 + numero2
+
+if (soma > 100) {
+    console.log("A soma é maior que 100")
+    
+} else {
+    console.log("A soma é menor ou igual a 100")
+}
