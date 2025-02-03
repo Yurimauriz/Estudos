bb58d467f1dc92fa1070599bcdc09f3a63bfe68f
FRANCISCO YURI DA SILVA MAURIZ <yurimauriz.dev@gmail.com>
2025-02-03 15:28:59 -0300

    Tipos em JavaSciprt: String

---
+++ fundamentos_js/strings.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)) // charAt é uma função para descobri a posição de uma letra
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf("3"))
+
+console.log(escola.substring(1)) // substring é uma função para descobrir a posição de uma letra
+console.log(escola.substring(0, 3))
+
+console.log("Escola ".concat(escola).concat("!!"))
+//console.log("Escola " + escola + "!!") // concat é uma função para concatenar strings
+
+console.log(escola.replace(3, "e")) // replace é uma função para trocar uma letra por outra
+
+console.log('Ana,Maria,pedro'.split(","))
+
